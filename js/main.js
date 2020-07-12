849ad0424af1c12a84ec93f8f70eaed38a32bc0b
Loouis Low <loouis@gmail.com>
2020-07-12 22:28:39 +0800

    remove unused codes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,12 @@
 import setupHome from 'pages/home.js';
 import setupDemo from 'pages/demo.js';
+
 import setupSearch from 'components/search.js';
 import setupHovers from 'components/hover.js';
 import setupPrefetch from 'components/prefetch.js';
 import setupNavigation from 'components/navigation.js';
 
 import AnchorJS from 'anchor-js';
-
-// import Reveal from 'reveal.js';
-// import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
-// import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-// import MathJax from 'reveal.js/plugin/math/math.esm.js';
-// import Zoom from 'reveal.js/plugin/zoom/zoom.esm.js';
 
 const PAGE_ID = document.body.dataset.page;
 
@@ -58,45 +53,3 @@ function setupAnchors() {
 
 }
 
-// function setupInlineDecks() {
-
-//   // Inline example decks in the docs
-//   Array.from(document.querySelectorAll('.reveal-example')).forEach(deckElement => {
-
-//     // If this deck is paired with an input code block,
-//     // wrap them both in a shared container
-//     let code = deckElement.previousSibling;
-//     if (code.matches('pre')) {
-//       let wrapper = document.createElement('y');
-//       wrapper.className = 'reveal-example-wrapper';
-//       code.parentNode.insertBefore(wrapper, code);
-//       wrapper.appendChild(code);
-//       wrapper.appendChild(deckElement);
-//     }
-
-//     let config = {};
-
-//     if (deckElement.hasAttribute('data-config')) {
-//       config = JSON.parse(deckElement.dataset.config);
-//     }
-
-//     let deck = new Reveal(deckElement, {
-//       width: 900,
-//       height: 500,
-//       hash: false,
-//       respondToHashChanges: false,
-//       embedded: true,
-//       progress: false,
-//       keyboardCondition: 'focused',
-//       controlsTutorial: false,
-//       math: {
-//         config: 'TeX-AMS_HTML-full'
-//       },
-//       plugins: [Markdown, Highlight, Zoom, MathJax],
-//       ...config
-//     });
-//     deck.initialize();
-
-//   });
-
-// }
