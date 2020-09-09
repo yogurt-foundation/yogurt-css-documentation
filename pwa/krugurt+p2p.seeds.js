75e04ad1da5210fb37d3d7ee7d0a085020eddb95
Loouis Low <loouis@gmail.com>
2020-09-10 03:05:37 +0800

    (testing) p2p caching

--- pwa/krugurt+p2p.seeds.js
+++ pwa/krugurt+p2p.seeds.js
@@ -1,15 +1,5 @@
 document.security_sha1 = {
   'https://yogurt-css-documentation.netlify.app/': '9b4db8a6d43d75b7ff0fd42ce04901aa26d631cc',
   'https://yogurt-css-documentation.netlify.app/installation/': 'f46d9aac2cff8a4dc896b88a7f4583829fb44797',
-  'https://yogurt-css-documentation.netlify.app/utility-map/': '9d027d8e5fe854047ab69603caa90471a5e2dc02',
-  'https://yogurt-css-documentation.netlify.app/utility/': '763a2136e27ad9951c7217a5e66e52f161921c7c',
-  'https://yogurt-css-documentation.netlify.app/responsive/': 'dbc561a684f2f62bce2d4b71810b7aa2355f2195',
-  'https://yogurt-css-documentation.netlify.app/pseudo-class-variants/': 'df167da986d4cd392a07bba0f133fd1748719a37',
-  'https://yogurt-css-documentation.netlify.app/extract-utility-classes/': '09086094b6c72a3848b8e65272f4834ff3f092f5',
-  'https://yogurt-css-documentation.netlify.app/import-framework/': 'f3e806f879790c88d2b7289cb4a9d2a2ed1a6dd6',
-  'https://yogurt-css-documentation.netlify.app/refactoring-ui/': 'cb97c50abfa21b1739ebc2e33acb5ddb774a602c',
-  'https://yogurt-css-documentation.netlify.app/file-size-control/': 'b96862a4c90abec092148e1d44d901730c6c6aaf',
-  'https://yogurt-css-documentation.netlify.app/customize-utilities/': '55e2241f6360be513fa13bb464a9a35ef5f6af95',
-  'https://yogurt-css-documentation.netlify.app/plugin-api/': '25b55e85e181800214e1b6d488b78a0eb87ec481',
-  'https://yogurt-css-documentation.netlify.app/debug/': 'eee9faa6029b7d74d8fdc7e9fa46fc77462d0388'
+  'https://yogurt-css-documentation.netlify.app/utility-map/': '9d027d8e5fe854047ab69603caa90471a5e2dc02'
 }
