ff975597efba9017df04856c66c11481c987d33c
Loouis Low <loouis@gmail.com>
2020-10-23 02:31:52 +0800

    update version number to 1.1.1

--- cm/template.js
+++ cm/template.js
@@ -1,4 +1,4 @@
-var PrototypeTemplate = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"><link defer href="https://unpkg.com/yogurt-css@1.1.0/dist/yogurt-1.1.0_solidcore.min.css" rel="stylesheet" rel="preload" as="style" media="all"></head>\n\n\
+var PrototypeTemplate = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"><link defer href="https://unpkg.com/yogurt-css@1.1.1/dist/yogurt-1.1.1_solidcore.min.css" rel="stylesheet" rel="preload" as="style" media="all"></head>\n\n\
   <!-- Begin Example -->\n\n\
   <body class="font-default bg-gray-100">\n\n\
     <y class="mx-auto my-8 max-w-xs">\n\
