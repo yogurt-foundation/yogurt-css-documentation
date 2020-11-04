7162f3074514e48d0a31c4114b5a0eaefe885003
Loouis Low <loouis@gmail.com>
2020-11-05 06:14:23 +0800

    update `playground` template

--- cm/template.js
+++ cm/template.js
@@ -2,16 +2,16 @@ var playgroundTemplate = '<!DOCTYPE html><html ><head><meta charset="utf-8"><met
   <!-- Change framework version from 1.1.0 to the latest -->\n\
   <link defer href="../yogurt-1.1.2_solidcore.min.css" rel="stylesheet" rel="preload" as="style" media="all"></head>\n\n\
   <!-- Begin Example -->\n\n\
-  <body class="font-default antialiased text-gray-600 bg-gray-100">\n\n\
-    <y class="h-screen flex flex-col justify-center items-center">\n\
-      <y class="pb-10 text-xl animation fade-in-up delay-2">\n\
+  <body class="font-default antialiased text-gray-600 (dark)text-gray-300 bg-gray-100 (dark)bg-gray-800">\n\n\
+    <y class="h-screen (xs)px-4 flex flex-col justify-center items-center">\n\
+      <y class="pb-10 text-2xl animation fade-in-up delay-2">\n\
         Welcome to\n\
         <span class="inline-block font-semibold underline">\n\
           Yogurtland\n\
         </span>\n\
         playground\n\
       </y>\n\
-      <y class="max-w-xs p-10 flex justify-around items-center bg-white rounded-lg shadow-dreamy w-full animation fade-in-up overflow-hidden">\n\
+      <y class="(xs)max-w-full (sm)max-w-xs p-10 flex justify-around items-center bg-white (dark)bg-charcoal-900 rounded-lg shadow-dreamy w-full animation fade-in-up overflow-hidden">\n\
         <y class="h-20 w-20 bg-gray-600 rounded-full"></y>\n\
         <y class="text-xl text-gray-600">\n\
          	Hello World!\n\
